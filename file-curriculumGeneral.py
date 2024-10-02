8c1594565c10d2a22eff8cc040b50976131a51b4
zoRich07 <sujinan7346@gmail.com>
2024-10-02 20:47:31 +0700

    update

--- file-curriculumGeneral.py
+++ file-curriculumGeneral.py
@@ -11,26 +11,23 @@ mainFile = client.open('Main')
 
 # เลือกชีท Curriculum
 curriculumSheet = mainFile.worksheet('Curriculum')
-curriculumData = curriculumSheet.range('B3:B10')
+curriculumData = [row[1] for row in curriculumSheet.get_all_values()[2:] if row[1]]
+
 
 newCurriculumFile = client.open('Curriculum_General Education Program')
 
 # สร้างชีทใหม่สำหรับแต่ละหลักสูตร
-for i, cell in enumerate(curriculumData):
-    courseID = cell.value
+for i, courseID in enumerate(curriculumData):
     newSheetName = f'{courseID}'
-    
-    # เพิ่มชีทใหม่ด้วยจำนวนแถว 20 และจำนวนคอลัมน์ 12
-    newSheet = newCurriculumFile.add_worksheet(title=newSheetName, rows='20', cols='12')
-    
+    newSheet = newCurriculumFile.add_worksheet(title=newSheetName, rows='20', cols='12') 
     headers = ['รหัสวิชา', 'ชื่อวิชา', 'หมวดหมู่รายวิชา', 'หน่วยกิต (ทฤษฎี-ปฏิบัติ-ศึกษาด้วยตนเอง)', 'คาบเรียน (บรรยาย)', 'คาบเรียน (ปฏิบัติ)', 'วันเรียนบรรยาย', 'คาบบรรยาย(เริ่ม)', 'คาบบรรยาย(จบ)', 'วันเรียนปฎิบัติ', 'คาบบรรปฎิบัติ(เริ่ม)', 'คาบปฎิบัติ(จบ)']
     newSheet.insert_row(headers, index=1)
 
     # หน่วงเวลา 1 วินาที
-    time.sleep(1)
+    time.sleep(1)  
 
     # ใช้ batch update แทนการอัปเดตทีละเซลล์
-    blank_data = [[''] * len(headers) for _ in range(19)]  # 19 แถวแทนช่วงจากแถว 2 ถึง 20
-    newSheet.update('A2:L20',blank_data)
+    blank_data = [[''] * len(headers) for _ in range(19)]
+    newSheet.update('A2:L20', blank_data)
 
 print("Success!")
